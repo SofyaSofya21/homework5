9df97e17f194f390bd1f14c5a05fe03253c3f090
Sofya <sofyaguseva1@gmail.comgit>
2022-11-26 16:18:41 +0100

    Task34 finished

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.WriteLine("Введите желаемую длину массива");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int minValue = 100;
+int maxValue = 999;
+
+int[] numbers = new int[size];
+numbers = FillArrayRandom(numbers, minValue, maxValue);
+
+int count = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if(numbers[i]%2 == 0)
+        count++;
+}
+
+Console.Write("["+String.Join(", ", numbers)+ "] -> "+ count);
+Console.WriteLine();
+
+
+int[] FillArrayRandom(int[] array, int min, int max)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    int i = 0;
+    Console.Write("[");
+    for (i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[i]}] ");
+    Console.WriteLine();
+}
+
+
